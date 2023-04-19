0588ada8cd1084ee60a793b73fec8eb6a73c2bfa
Orion Palaquibay <23282980+orionpal@users.noreply.github.com>
2023-04-19 16:56:23 -0400

    file generation

--- generation.ts
+++ generation.ts
@@ -1,14 +1,12 @@
-import * as moment from "moment";
 import { App } from "obsidian";
 
 // hope this is fine
 export async function generateQuestLog(app: App) {
 
-    const date = moment().format('YYYY-MM-DD');
-    const filename = `${date}.md`;
+    const date = new Date().toISOString().substr(0, 10);
     // For now let's just create a new note
     // Okay woo this workss (make sure file extension is included)
-    const file = app.vault.create("quest-log.md", "Hey I'm a generated file!");
+    const file = app.vault.create(`${date}.quest-log.md`, "Hey I'm a generated file! I totally have today's date");
 
     return file
 }
